2168ebbb8f98953ed18120a25296d9ee942825d6
Edgar Gunther <edgaragunther@gmail.com>
2023-06-28 16:57:07 -0700

    New Map and More juice added

---
+++ Movement_Tools/Assets/MovementPackage/Scripts/Player/Camera/CameraPhysics.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPhysics : MonoBehaviour
+{
+    public PlayerController player;
+    SecondOrderDynamics movementDynamics;
+    SecondOrderDynamics rotationDynamics;
+
+    [Header("Centering")]
+    public float centerForce = 10f;
+    public float snappiness = 10f;
+    public float randomForce = 50f;
+
+    [Header("Second Order Dynamics")]
+    public float movementFrequency = 2;
+    public float movementDampingCoefficient = .8f;
+    public float movementResponse = .5f;
+    public float movementScale = 1f;
+
+    public float rotationFrequency = 2;
+    public float rotationDampingCoefficient = .8f;
+    public float rotationResponse = .5f;
+    public float rotationScale = 1f;
+
+
+    Vector3 targetRecoilRotation = Vector3.zero;
+    Vector3 currentRecoilRotation = Vector3.zero;
+    Vector3 leaningVector = Vector3.zero;
+
+    Vector3 movementInputVector = Vector3.zero;
+    Vector3 rotationInputVector = Vector3.zero;
+
+    [HideInInspector] public Vector3 offsetPosition;
+    [HideInInspector] public Vector3 offsetRotation;
+
+    private void Start()
+    {
+        player = PlayerController.player;
+        movementDynamics = new SecondOrderDynamics();
+        movementDynamics.SecondOrderDynamicsFunction(movementFrequency, movementDampingCoefficient, movementResponse, transform.localPosition);
+
+        rotationDynamics = new SecondOrderDynamics();
+        rotationDynamics.SecondOrderDynamicsFunction(rotationFrequency, rotationDampingCoefficient, rotationResponse, Vector3.zero);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ApplyForce();
+    }
+
+    public void ApplyForce()
+    {
+
+        //MOVEMENT DYNAMICS
+        //------------------------------------------------------------------------------------------------------------------------
+        movementInputVector = Vector3.zero;
+        movementInputVector += transform.InverseTransformDirection(new Vector3(0, player.Controller.velocity.normalized.y, 0));
+
+        movementInputVector += offsetPosition;
+        movementInputVector = movementDynamics.UpdateDynamics(Time.fixedDeltaTime, movementInputVector);
+        transform.localPosition = movementScale * movementInputVector;
+        //------------------------------------------------------------------------------------------------------------------------
+
+        //ROTATION DYNAMICS
+        //------------------------------------------------------------------------------------------------------------------------
+        rotationInputVector = Vector3.zero;
+        rotationInputVector += transform.InverseTransformDirection(Vector3.Cross(-new Vector3(player.moveVec.x, 0, player.moveVec.z), Vector3.up));
+
+        rotationInputVector += offsetRotation;
+        rotationInputVector = rotationDynamics.UpdateDynamics(Time.fixedDeltaTime, rotationInputVector) * rotationScale;
+
+        //RECOIL
+        targetRecoilRotation = Vector3.Lerp(targetRecoilRotation, Vector3.zero, centerForce * Time.fixedDeltaTime);
+        currentRecoilRotation = Vector3.Slerp(currentRecoilRotation, targetRecoilRotation, snappiness * Time.fixedDeltaTime);
+        transform.localRotation = Quaternion.Euler(currentRecoilRotation + rotationInputVector);
+        //------------------------------------------------------------------------------------------------------------------------
+    }
+    public void AddForce(float force, Vector3 axis)
+    {
+        targetRecoilRotation += axis * force;
+    }
+
+    [ContextMenu("Apply Random Force")]
+    public void ApplyRandomForce()
+    {
+        AddForce(randomForce, Random.insideUnitSphere);
+    }
+}
---
+++ Movement_Tools/Assets/MovementPackage/Scripts/Player/Custom/WallClimb.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(PlayerJump))]
+[RequireComponent(typeof(PlayerCrouch))]
+public class WallClimb : MonoBehaviour
+{
+    PlayerController _player;
+    PlayerJump _jumpEvent;
+    PlayerCrouch _crouchEvent;
+    Transform _camera;
+
+    public float wallClimbForce;
+    public float maxWallClimbSpeed = 5f;
+
+    [Tooltip("max angle you can turn away from the wall before wall climbing turns off")]
+    [Range(0,90)]public float wallClimbBreakAngle = 45;
+    float wcbaCoefficient;
+
+    //private variables
+    bool _waitingForEnd = false;
+    Vector3 velocityCap = Vector3.zero;
+    float dot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<PlayerController>();
+        _jumpEvent = GetComponent<PlayerJump>();
+        _crouchEvent = GetComponent<PlayerCrouch>();
+        _camera = _player.Cam.transform;
+
+        wcbaCoefficient = -1 + (wallClimbBreakAngle / 90);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        print(Vector3.Dot(hit.normal, _camera.forward) + " dot prod");
+
+        if (_player.grounded || _crouchEvent.crouching)
+            return;
+
+        if (!_waitingForEnd && !_crouchEvent.crouching)
+            StartCoroutine(WaitForCollisionEnd(hit));
+
+    }
+
+    private IEnumerator WaitForCollisionEnd(ControllerColliderHit hit)
+    {
+        _waitingForEnd = true;
+        Vector2 excludeYAxis = new Vector2(hit.normal.x, hit.normal.z).normalized;
+        Vector2 cameraNoY = new Vector2(_camera.forward.x, _camera.forward.z).normalized;
+        dot = Vector2.Dot(excludeYAxis, cameraNoY);
+        yield return null;
+        while (_player.Controller.collisionFlags != CollisionFlags.None && _player.Controller.collisionFlags != CollisionFlags.Below && !_crouchEvent.crouching)
+        {
+            excludeYAxis.x = hit.normal.x; excludeYAxis.y = hit.normal.z;
+            cameraNoY.x = _camera.forward.x; cameraNoY.y = _camera.forward.z;
+            dot = Vector2.Dot(excludeYAxis.normalized, cameraNoY.normalized);
+            if (dot > wcbaCoefficient)
+                break;
+
+            velocityCap = _player.moveVec;
+            velocityCap.y = Mathf.Clamp(velocityCap.y + (wallClimbForce * Time.deltaTime), -Mathf.Infinity, maxWallClimbSpeed);
+            _player.SetForce(velocityCap);
+
+            yield return null;
+        }
+        _jumpEvent.RefreshJump();
+        _waitingForEnd = false;
+    }
+}
